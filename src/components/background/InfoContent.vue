8b29da68fd1be822a723741cf708cddfe9b88a1f
Benjamin LACROIX <blacroix@xebia.fr>
2020-03-22 22:02:11 +0100

    design(info): better layout

--- src/components/background/InfoContent.vue
+++ src/components/background/InfoContent.vue
@@ -1,86 +1,114 @@
 <template>
-  <div class="info-content" :class="mode">
-    <div v-if="background" class="info-content__background">
-      <Background :background="background"/>
-    </div>
-    <div v-if="posts" :style="{order: postsOrder}" class="info-content__posts">
-      <Posts :posts="posts"/>
-    </div>
-    <div v-if="talks" :style="{order: talksOrder}" class="info-content__talks">
-      <Talks :talks="talks"/>
-    </div>
-  </div>
+  <ul class="info-content" :class="mode">
+    <li class="info-content__step info-content__title">Parcours</li>
+    <li v-for="step in background" :key="step.description" class="info-content__step">
+      <div class="info-content__step-inside">
+        <span class="info-content__year">{{step.year}}</span> - <span class="info-content__background-name">{{step.name}}</span>
+        <div>
+          {{step.description}}
+        </div>
+      </div>
+    </li>
+    <li v-if="posts" class="info-content__step info-content__title">Publications</li>
+    <li v-for="post in posts" :key="post.title" class="info-content__step">
+      <div class="info-content__step-inside">
+        <span class="info-content__year">{{post.year}}</span> - <span class="info-content__posts-target">{{post.postTarget}}</span>
+        <div>
+          "{{post.name}}"
+        </div>
+      </div>
+    </li>
+    <li v-if="talks" class="info-content__step info-content__title">Speaker</li>
+    <li v-for="talk in talks" :key="talk.subject" class="info-content__step">
+      <div class="info-content__step-inside">
+        <span class="info-content__year">{{talk.year}}</span> - {{talk.subject}}
+        <div>
+          <span class="info-content__talks-conference">{{talk.conference}}</span> <span v-if="talk.place">({{talk.place}})</span>
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
-  import Background from '@/components/background/Background';
-  import Talks from '@/components/background/Talks';
-  import Posts from '@/components/background/Posts';
-
   export default {
     name: 'InfoContent',
-    components: {Posts, Talks, Background},
     props: ['background', 'talks', 'posts'],
     computed: {
-      postsOrder() {
-        if (this.talks && this.posts) {
-          if (this.talks.length > this.posts.length) {
-            return 2
-          }
-        }
-        return 3;
-      },
-      talksOrder() {
-        if (this.talks && this.posts) {
-          if (this.talks.length > this.posts.length) {
-            return 3
-          }
-        }
-        return 2;
-      },
       mode() {
-        const talkCount = this.talks ? this.talks.length : 0;
-        const postCount = this.posts ? this.posts.length : 0;
-        const stepCount = this.background ? this.background.length : 0;
-        const count = talkCount + postCount + stepCount;
-        if (count < 6) {
-          return 'info-content--2column-small';
-        } else if (count < 11) {
-          return 'info-content--2column';
+        const bLength = this.background && this.background.length ? this.background.length : 0;
+        const tLength = this.talks && this.talks.length ? this.talks.length : 0;
+        const pLength = this.posts && this.posts.length ? this.posts.length : 0;
+        let length = bLength + tLength + pLength;
+        let mode = '';
+        if (length > 6) {
+          mode += ' info-content--2columns ';
         }
-        return '';
+        if (length > 10) {
+          mode += ' info-content--many ';
+        }
+        return mode;
       }
-    }
+    },
   }
 </script>
 
 <style scoped lang="scss">
   .info-content {
-    padding: 50px;
+    padding: 50px 62px;
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
-    height: 800px;
+    max-height: 550px;
 
-    &--2column-small {
-      height: 300px;
-      page-break-after: avoid;
+    $self: &;
+
+    &--2columns {
+      > #{$self}__step {
+        width: 44%;
+      }
     }
 
-    &--2column {
-      height: 500px;
+    &--many {
+      max-height: 850px;
     }
 
-    &__background {
-      width: 50%;
+    &__step {
+      margin: 5px 25px;
     }
 
-    &__talks {
-      width: 50%;
+    &__title {
+      font-weight: 700;
+      font-size: 1.7em;
+      margin-bottom: 15px;
+      margin-top: 25px;
+
+      &:first-of-type {
+        margin-top: 0;
+      }
+
+      &::before {
+        content: '';
+        margin-left: -1.5em;
+      }
     }
 
-    &__posts {
-      width: 50%;
+    &__step-inside {
+      display: inline-block;
+    }
+
+    &__background-name {
+      font-weight: 700;
+      color: #fe414d;
+    }
+
+    &__year {
+      font-weight: 700;
+    }
+
+    &__posts-target, &__talks-conference {
+      font-weight: 700;
+      color: #04a0ff;
     }
   }
 </style>
