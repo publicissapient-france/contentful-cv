75ee8a8eb0670d6d064d5a2598cb6cad93db9bb2
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2022-12-06 23:03:04 +0100

    🔒️check user auth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {getUserId, UserIdNotFoundError} from "./auth";
 
-export const helloWorld = functions
+initializeApp();
+
+// noinspection JSUnusedGlobalSymbols
+export const cv = functions
     .region("europe-central2")
-    .https.onRequest((request, response) => {
-      functions.logger.info("Hello logs!", {structuredData: true});
-      response.send("Hello from Firebase!");
+    .https.onRequest(async (request, response) => {
+      try {
+        const userId = await getUserId(request.get("Authorization"));
+        response.status(200).send(userId);
+      } catch (e) {
+        if (e instanceof UserIdNotFoundError) {
+          functions.logger.info(e, {structuredData: true});
+        } else {
+          functions.logger.warn(e, {structuredData: true});
+        }
+        response.sendStatus(401);
+      }
     });
