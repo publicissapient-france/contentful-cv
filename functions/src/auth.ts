6d9af6f2683558795903ec191466b037f0603d3b
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2022-12-07 21:23:23 +0100

    🔒️check user authorized

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -1,19 +1,21 @@
 import {auth} from "firebase-admin";
-
-/** */
-export class UserIdNotFoundError extends Error {
-  message = "Cannot find user id";
-}
+import {error} from "firebase-functions/logger";
 
 export const getUserId = async (bearer: string | undefined) => {
   if (!bearer || !bearer.startsWith("Bearer ")) {
-    throw new UserIdNotFoundError();
+    return null;
   }
-  const idToken = bearer.split("Bearer ")[1];
-  const decodedIdToken = await auth().verifyIdToken(idToken);
-
-  if (!decodedIdToken.email) {
-    throw new UserIdNotFoundError();
+  try {
+    const idToken = bearer.split("Bearer ")[1];
+    const decodedIdToken = await auth().verifyIdToken(idToken);
+    if (decodedIdToken.email) {
+      return decodedIdToken.email;
+    }
+  } catch (e) {
+    error(e);
   }
-  return decodedIdToken.email;
+  return null;
 };
+
+export const isUserAuthorized = (userId: string) =>
+  userId.endsWith("@publicissapient.com");
