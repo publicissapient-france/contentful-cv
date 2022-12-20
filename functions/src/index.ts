5525aff3f58cf5ff8037c043747fc435ee56e2ed
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2022-12-20 22:15:59 +0100

    ✨ cv by id

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,12 +14,15 @@ export const fcv = functions
     .https.onRequest((request, response) => {
       corsHandler(request, response, async () => {
         const userId = await getUserId(request.get("Authorization"));
-        const locale = (request.query.locale || "fr") as string;
         if (!userId || !isUserAuthorized(userId)) {
           response.sendStatus(403);
           return;
         }
-        const cv = await getCv(userId, locale);
+
+        const locale = (request.query.locale || "fr") as string;
+        const id = request.path.split("/")[1];
+        const cv = await getCv(id, locale);
+
         response.status(200).send(cv);
       });
     });
