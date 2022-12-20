0c0506ea5f9d41aa7c64ebdf9395fd2c6a1a3540
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2022-12-20 21:54:23 +0100

    ✨ fcv

--- functions/src/cv.ts
+++ functions/src/cv.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const getCv = async (userId: string) => {
-  const {data} = await axios.get(`https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE}/environments/master/entries/?content_type=curriculumVitae&include=2&fields.id=${userId}`, {
+export const getCv = async (userId: string, locale: string) => {
+  const {data} = await axios.get(`https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE}/environments/master/entries/?content_type=curriculumVitae&include=2&fields.id=${userId}&locale=${locale}`, {
     headers: {
       Authorization: `Bearer ${process.env.CONTENTFUL_TOKEN}`,
     },
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,25 @@
 import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
+import * as cors from "cors";
 import {getUserId, isUserAuthorized} from "./auth";
 import {getCv} from "./cv";
+
+const corsHandler = cors({origin: true});
 
 initializeApp();
 
 // noinspection JSUnusedGlobalSymbols
-export const cv = functions
+export const fcv = functions
     .region("europe-central2")
-    .https.onRequest(async (request, response) => {
-      const userId = await getUserId(request.get("Authorization"));
-      if (!userId || !isUserAuthorized(userId)) {
-        response.sendStatus(403);
-        return;
-      }
-      const cv = await getCv(userId);
-      response.status(200).send(cv);
+    .https.onRequest((request, response) => {
+      corsHandler(request, response, async () => {
+        const userId = await getUserId(request.get("Authorization"));
+        const locale = (request.query.locale || "fr") as string;
+        if (!userId || !isUserAuthorized(userId)) {
+          response.sendStatus(403);
+          return;
+        }
+        const cv = await getCv(userId, locale);
+        response.status(200).send(cv);
+      });
     });
